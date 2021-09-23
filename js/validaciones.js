58816404969ebf6914508f63d7244b660731f1a2
SergioPerez <sergioandresperez92@hotmail.com>
2021-09-22 22:05:49 -0500

    Primer Sprint Finalizado

---
+++ js/validaciones.js
@@ -0,0 +1,11 @@
+function validar_nombre_usuario(string){
+    // Implementación
+}
+
+function validar_edad_usuario(edad){
+    // Implementación
+}
+
+function validar_contrasena(string){
+    // Implementación
+}
